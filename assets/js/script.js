ef8a848921eeb2c4701a20f13ef890b3af1ea736
Franklin <franklin.c.brown@gmail.com>
2022-02-11 23:05:09 -0800

    added endpoints

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,6 @@ var APIkey = '836a020d141462da8b2466b0c5126868';
 var limit = 1;
 var cityName = document.getElementById("searchInput").value;
 
-var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + APIkey;
-
 var searchInput = document.querySelector('#searchInput');
 var searchBtn = document.querySelector('#searchBtn');
 
@@ -13,25 +11,31 @@ var formSubmitHandler = function(event) {
   
     // get value from input element
     cityName = searchInput.value.trim();
+
+    document.querySelector(".place").textContent = cityName;
   
     if (cityName) {
+        //console.log(cityName); //this works
         getData(cityName);
   
       // clear old content
-        //searchInput.value = "";
-   // }   else {
-    //    alert("Please enter a city");
+        searchInput.value = "";
+    }   else {
+        alert("Please enter a city");
     }
 };
 
 var getData = function() {
-    console.log('I am working');
+    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + APIkey;
+
+    // console.log(geoApiUrl); // now this works
     fetch(geoApiUrl).
     then(function(response) {
+        //console.log(response); //this works
         return response.json()
     })
     .then(function(data) {
-        console.log(data);
+        //console.log(data); //this works
 
         var lat = data[0].lat;
         var lon = data[0].lon;
@@ -42,15 +46,49 @@ var getData = function() {
             return response.json()
         })
         .then(function(data) {
-            console.log(data);
+            console.log(data); //this works
+
+            for (var i = 0; i < data.daily.length; i++) {
+            //Heute
+            document.getElementById("todayTemp").textContent = data.daily[i].temp.day + "F";
+            document.getElementById("todayWind").textContent = data.daily[i].wind_speed + ' mph';
+            document.getElementById("todayHum").textContent = data.daily[i].humidity + '%';
+            document.getElementById("todayUV").textContent = data.daily[i].uvi;
+
+            
+            //Morgen
+            document.getElementById("day1Temp").textContent = data.daily[1].temp.day + "F";
+            document.getElementById("day1Wind").textContent = data.daily[1].wind_speed + ' mph';
+            document.getElementById("day1Hum").textContent = data.daily[1].humidity + '%';
+
+            //Uebermorgen
+            document.getElementById("day2Temp").textContent = data.daily[2].temp.day + "F";
+            document.getElementById("day2Wind").textContent = data.daily[2].wind_speed + ' mph';
+            document.getElementById("day2Hum").textContent = data.daily[2].humidity + '%';
+
+            //3ten Tag
+            document.getElementById("day3Temp").textContent = data.daily[3].temp.day + "F";
+            document.getElementById("day3Wind").textContent = data.daily[3].wind_speed + ' mph';
+            document.getElementById("day3Hum").textContent = data.daily[3].humidity + '%';
+
+            //4ten Tag
+            document.getElementById("day4Temp").textContent = data.daily[4].temp.day + "F";
+            document.getElementById("day4Wind").textContent = data.daily[4].wind_speed + ' mph';
+            document.getElementById("day4Hum").textContent = data.daily[4].humidity + '%';
+
+            //5ten Tag
+            document.getElementById("day5Temp").textContent = data.daily[5].temp.day + "F";
+            document.getElementById("day5Wind").textContent = data.daily[5].wind_speed + ' mph';
+            document.getElementById("day5Hum").textContent = data.daily[5].humidity + '%';
+            }
+            
         })
     })
 };
 
+
+
 searchBtn.addEventListener('click', formSubmitHandler);
-
-getData();
-
 
 // city_name, current.temp, current.humidity, current.wind_speed, hourly.uvi
 
@@ -65,7 +103,7 @@ var pastHandler = function(event) {
     event.preventDefault();
 
     if (cityName === "") {
-        console.log("empty field");
+        //console.log("empty field"); //this works
         return false;
     } else {
         var listingObj = {
@@ -73,9 +111,8 @@ var pastHandler = function(event) {
         };
     };    
 
-    console.log(listingObj);
+    //console.log(listingObj); //this works
     createListingEl(listingObj);
-    // all of this works
 };
 
 // adds search to side nav
@@ -84,14 +121,14 @@ var createListingEl = function(listingObj) {
     listItemEl.className = "col-12";
     listItemEl.setAttribute("list-id", idCounter);
   
-    console.log("list item created"); //this works
+    //console.log("list item created"); //this works
     searches.appendChild(listItemEl);
     
     var listBtnEl = document.createElement('button');
     listBtnEl.className = "btn cityBtn col-12";
 
     listBtnEl.textContent = listingObj.city;  
-    console.log(listBtnEl); //this works
+    // console.log(listBtnEl); //this works
     listItemEl.appendChild(listBtnEl);
 
     listingObj.id = idCounter;
@@ -102,13 +139,13 @@ var createListingEl = function(listingObj) {
 
     // gives each listing an id
     idCounter++;
-    console.log(idCounter);
+    //console.log(idCounter);// this works
 };
 
 // saves searches to local storage
 var saveListing = function() {
     localStorage.setItem("listings", JSON.stringify(listings));
-    console.log("listing saved!")
+    // console.log("listing saved!") //this works
 };
 
 //retreives from storage
@@ -130,3 +167,22 @@ var loadListing = function() {
 document.querySelector("#searchBtn").addEventListener('click', pastHandler);
 
 loadListing();
+
+document.querySelector(".date").textContent = "(" + moment().calendar('DD/MM/YYYY') + ")";
+document.querySelector(".place").textContent = cityName; // this needs to be taken from the API?
+document.getElementById('day1Date').textContent = moment().add(1,'d').calendar('MM/DD/YYYY');
+document.getElementById('day2Date').textContent = moment().add(2,'d').calendar('MM/DD/YYYY');
+document.getElementById('day3Date').textContent = moment().add(3,'d').calendar('MM/DD/YYYY');
+document.getElementById('day4Date').textContent = moment().add(4,'d').calendar('MM/DD/YYYY');
+document.getElementById('day5Date').textContent = moment().add(5,'d').calendar('MM/DD/YYYY');
+
+
+/*
+if (document.getElementById("todayUV").textContent < 2) {
+    document.getElementById("todayUV").classList("col-1 favorable");
+    } else if (document.getElementById("todayUV").textContent > 2 && document.getElementById("todayUV").textContent < 4) {
+        document.getElementById("todayUV").classList("col-1 moderate");
+    } else {
+        document.getElementById("todayUV").classList("col-1 severe");
+    }
+*/
